5a8071223aa69fa2ec3d1064d625a4abde0e653b
Kamil Glazer <k.glazer.kamil@interia.pl>
2024-03-08 11:48:05 +0100

    Initial commit

--- src/main/java/efs/task/syntax/GuessNumberGame.java
+++ src/main/java/efs/task/syntax/GuessNumberGame.java
@@ -1,6 +1,14 @@
 package efs.task.syntax;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumberGame {
+
+    private int M;
+    private double L;
+    private int number;
 
     //Do not modify main method
     public static void main(String[] args) {
@@ -13,10 +21,70 @@ public class GuessNumberGame {
     }
 
     public GuessNumberGame(String argument) {
-        //TODO: Implement the constructor
+
+        try{
+            this.M = Integer.parseInt(argument);
+        }catch(NumberFormatException e){
+            System.out.println("'"+argument+"'"+ " to "+ UsefulConstants.WRONG_ARGUMENT + " - to nie liczba");
+            throw new IllegalArgumentException();
+        }
+
+        if(M < 1 || M > 400) {
+            System.out.println(argument + " to " + UsefulConstants.WRONG_ARGUMENT + " - jest spoza zakresu <1,"+UsefulConstants.MAX_UPPER_BOUND+">");
+            throw new IllegalArgumentException();
+        }
+
+        Random random = new Random();
+        this.number = random.nextInt(this.M)+1;
+        this.L = Math.floor(Math.log((double)this.M)/Math.log(2)) + 1;
     }
 
     public void play() {
-        //TODO: Implement the method that executes the game session
+
+        int trial = 0;
+        String[] tab = new String[(int)this.L];
+        int guess;
+
+        Arrays.fill(tab,".");
+
+
+        System.out.println("Zagrajmy. Zgadnij liczbę z zakresu <1,"+ M +">");
+        while (true){
+
+            Scanner scanner = new Scanner(System.in);
+
+            if(trial >= (int)this.L){
+                System.out.println(UsefulConstants.UNFORTUNATELY+ ", wyczerpałeś limit prób ("+(int)this.L+") do odgadnięcia liczby "+this.number);
+                break;
+            }
+
+            tab[trial] = "*";
+
+            System.out.print("Twoje próby: [");
+            for(int i=0;i< tab.length;i++){
+                System.out.print(tab[i]);
+            }
+            System.out.println("]");
+
+            System.out.println(UsefulConstants.GIVE_ME+" liczbę : ");
+
+            try{
+                guess = scanner.nextInt();
+                if(guess > number){
+                    System.out.println("To "+UsefulConstants.TO_MUCH);
+                }else if(guess < number){
+                    System.out.println("To "+UsefulConstants.TO_LESS);
+                }else{
+                    System.out.println("To "+UsefulConstants.YES);
+                    System.out.println(UsefulConstants.CONGRATULATIONS+", "+(trial+1)+" - tyle prób zajeło Ci odgadnięcie liczby "+this.number);
+                    break;
+                }
+            }catch (Exception e){
+                System.out.println("Hmm, '"+scanner.next()+"' to "+UsefulConstants.NOT_A_NUMBER);
+            }
+
+            trial++;
+        }
+
     }
 }
